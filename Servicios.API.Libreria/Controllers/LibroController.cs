d114352a39608aebc0d933b01e3a672528a23536
Abraham Azael <abraham@truiki.com>
2022-01-17 02:20:39 -0600

    NetMicroservice Add: LibroController y LibroEntity

---
+++ Servicios.API.Libreria/Controllers/LibroController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Servicios.API.Libreria.Core.Entities;
+using Servicios.API.Libreria.Repository;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Servicios.API.Libreria.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LibroController : ControllerBase
+    {
+        private readonly IMongoRepository<LibroEntity> _libroRepository;
+        public LibroController(IMongoRepository<LibroEntity> libroRepository)
+        {
+            _libroRepository = libroRepository;
+        }
+
+        [HttpPost]
+        public async Task CreateLibro(LibroEntity libro)
+        {
+            await _libroRepository.InsertDocument(libro);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<LibroEntity>>> GetAllLibros()
+        {
+            return Ok(await _libroRepository.GetAllAsync());
+        }
+
+        [HttpPost("pagination")]
+        public async Task<ActionResult<PaginationEntity<LibroEntity>>> Pagination(PaginationEntity<LibroEntity> pagination)
+        {
+            var resultados = await _libroRepository.PaginationByFilter(pagination);
+            return Ok(resultados);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<LibroEntity>> GetLibroById(string id)
+        {
+            var libro = await _libroRepository.GetById(id);
+            return Ok(libro);
+        }
+    }
+}
---
+++ Servicios.API.Libreria/Core/Entities/LibroEntity.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace Servicios.API.Libreria.Core.Entities
+{
+    [BsonCollection("Libro")]
+    public class LibroEntity : MongoDocument
+    {
+        [BsonElement("titulo")]
+        public string Titulo { get; set; }
+
+        [BsonElement("descripcion")]
+        public string Descripcion { get; set; }
+
+        [BsonElement("precio")]
+        public int Precio { get; set; }
+
+        [BsonElement("fechaPublicacion")]
+        public DateTime FechaPublicacion { get; set; }
+
+        [BsonElement("autor")]
+        public AutorEntity Autor { get; set; }
+    }
+}
